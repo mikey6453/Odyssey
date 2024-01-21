dba521ccb564565ce3b3700b8a0cb4bfb13906f0
Michael <tvq3bn@virginia.edu>
2024-01-20 23:50:30 -0500

    added labels for rating and types

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 import useStyles from './ListStyles';
 
 const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('resturants');
+    const [rating, setRating] = useState('');
+
+    const places = [
+        { name: 'Cool place'},
+        { name: 'Best Beer'},
+        { name: 'Best Steak'},
+    ];
+
     return (
-        <h1>List</h1>
+        
+        <div className={classes.container}>
+            <Typography variant='h4'>Resturants, Hotels, & Attractions around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Types</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value='resturants'>Resturants</MenuItem>
+                    <MenuItem value='hotels'>Hotels</MenuItem>
+                    <MenuItem value='attractions'>Attractions</MenuItem>
+                </Select> 
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     );
 }
 
