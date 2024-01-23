52bf82771af69855e0eefd09650451378aa1c19c
Michael <tvq3bn@virginia.edu>
2024-01-22 21:52:20 -0500

    show rating on card and location details

--- src/components/LocationDetails/LocationDetails.jsx
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -21,8 +21,8 @@ const LocationDetails = ({ place }) => {
                     {place.name}
                 </Typography>
                 <Box display="flex" justifyContent="space-between">
-                    <Typography variant="subtitle1">Price</Typography>
-                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                    <Rating value={Number(place.rating)} readOnly/>
+                    <Typography gutterBottom variant="subtitle1">out of {place.num_reviews}</Typography>
                 </Box>
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant="subtitle1">Ranking</Typography>
